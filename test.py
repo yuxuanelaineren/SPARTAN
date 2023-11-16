e9a6053967974a172ae9f9b443cc6880e2eeb4db
yuxuanelaineren <myyuxuanren@gmail.com>
2023-11-16 15:17:20 -0600

    cannot save as tiff

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import seaborn as sns
 from scipy import stats
 import numpy as np
 
+
+################################################################################################
 def get_tick_interval(data_range):
     """
     Calculate a reasonable tick interval based on the data range.
@@ -17,7 +19,7 @@ def get_tick_interval(data_range):
     - tick_interval: The calculated tick interval.
     """
     # Define the conditions and corresponding intervals
-    conditions = [0.5 * 6, 1 * 6, 2 * 6, 5 * 6]
+    conditions = [0.5 * 5, 1 * 5, 2 * 5, 5 * 5]
     intervals = [0.5, 1, 2, 5]
 
     # Find the appropriate interval based on the conditions
@@ -31,16 +33,18 @@ def get_tick_interval(data_range):
 
     return tick_interval
 
+
 # Set the directory path
 HIPS_dir_path = '/Volumes/rvmartin/Active/SPARTAN-shared/Analysis_Data/Master_files/'
 UV_dir_path = '/Users/renyuxuan/Desktop/Research/Black_Carbon/Analysis-10May2023_Joshin/Results/'
+Site_dir_path = '/Volumes/rvmartin/Active/SPARTAN-shared/Site_Sampling/'
 Out_dir_path = '/Users/renyuxuan/Desktop/Research/Black_Carbon/UV-Vis_HIPS_Comparison/'
 
 # Read the file
 merged_df = pd.read_excel(os.path.join(Out_dir_path, "BC_Comparison_HIPS_UV-Vis.xlsx"))
 
 # Drop rows where f_BC is greater than 1
-merged_df = merged_df.loc[merged_df['f_BC'] <= 1]
+merged_df = merged_df.loc[merged_df['f_BC_UV-Vis'] <= 1]
 
 # Rename to simplify coding
 merged_df.rename(columns={"BC_HIPS_(ug/m3)": "HIPS"}, inplace=True)
@@ -49,8 +53,20 @@ merged_df.rename(columns={"BC_UV-Vis_(ug/m3)": "UV-Vis"}, inplace=True)
 # Get a list of all unique site names
 site_names = merged_df['City'].unique()
 
+# Set the number of columns for the combined plot
+num_columns = 7
+
+# Calculate the number of rows needed
+num_rows = int(np.ceil(len(site_names) / num_columns))
+
+# Create a figure and axis with the specified layout
+fig, axes = plt.subplots(num_rows, num_columns, figsize=(16, 2 * num_rows))
+
+# Flatten the axes array to simplify iteration
+axes = axes.flatten()
+
 # Loop through each subplot and plot the data for the corresponding site
-for site_name in site_names:
+for i, site_name in enumerate(site_names):
     # Get the data for the current site
     site_data = merged_df.loc[merged_df['City'] == site_name]
 
@@ -58,28 +74,20 @@ for site_name in site_names:
     if len(site_data) < 2:
         continue
 
-    # Create a new figure and axis for each site
-    fig, ax = plt.subplots(figsize=(8, 6))
-
-    # Create scatter plot
-    scatterplot = sns.scatterplot(x='UV-Vis', y='HIPS', data=site_data, s=15, alpha=1, ax=ax)
+    # Create scatter plot on the current subplot
+    scatterplot = sns.scatterplot(x='UV-Vis', y='HIPS', data=site_data, s=15, alpha=1, ax=axes[i])
     scatterplot.set_facecolor('white')
     border_width = 1
     for spine in scatterplot.spines.values():
         spine.set_edgecolor('black')
         spine.set_linewidth(border_width)
-    scatterplot.grid(False)  # remove the grid
-
-    # Show x and y ticks for the current subplot
-    scatterplot.tick_params(axis='both', which='both', labelsize=10, length=4, width=1)
-    scatterplot.xaxis.set_tick_params(labelbottom=True)
-    scatterplot.yaxis.set_tick_params(labelleft=True)
+    scatterplot.grid(False)
 
     # Set axis limits for the current subplot
     x_min, x_max = site_data[['UV-Vis', 'HIPS']].min().min() - 0.2, site_data[['UV-Vis', 'HIPS']].max().max() + 0.5
     y_min, y_max = site_data[['UV-Vis', 'HIPS']].min().min() - 0.2, site_data[['UV-Vis', 'HIPS']].max().max() + 0.5
-    ax.set_xlim(x_min, x_max)
-    ax.set_ylim(y_min, y_max)
+    axes[i].set_xlim(x_min, x_max)
+    axes[i].set_ylim(y_min, y_max)
 
     # Calculate reasonable tick intervals based on the range of data
     x_range = x_max - x_min
@@ -91,18 +99,20 @@ for site_name in site_names:
     scatterplot.xaxis.set_major_locator(MultipleLocator(x_ticks_interval))
     scatterplot.yaxis.set_major_locator(MultipleLocator(y_ticks_interval))
 
-    # Set tick labels font to Arial
-    scatterplot.set_xticks(scatterplot.get_xticks())  # Set x tick locations explicitly
-    scatterplot.set_xticklabels(scatterplot.get_xticks(), fontdict={'fontname': 'Arial', 'fontsize': 10})
-    scatterplot.set_yticks(scatterplot.get_yticks())  # Set y tick locations explicitly
-    scatterplot.set_yticklabels(scatterplot.get_yticks(), fontdict={'fontname': 'Arial', 'fontsize': 10})
+    # Set x and y tick labels with a specific number of digits
+    scatterplot.set_xticks(scatterplot.get_xticks())
+    scatterplot.set_xticklabels([f'{tick:.2f}' for tick in scatterplot.get_xticks()],
+                                fontdict={'fontname': 'Arial', 'fontsize': 8})
+    scatterplot.set_yticks(scatterplot.get_yticks())
+    scatterplot.set_yticklabels([f'{tick:.2f}' for tick in scatterplot.get_yticks()],
+                                fontdict={'fontname': 'Arial', 'fontsize': 8})
 
     # Add 1:1 line with grey dash
-    ax.plot([x_min, x_max], [y_min, y_max], color='grey', linestyle='--', linewidth=1)
+    axes[i].plot([x_min, x_max], [y_min, y_max], color='grey', linestyle='--', linewidth=1)
 
     # Add the number of data points to the plot
     num_points = len(site_data)
-    ax.text(0.05, 0.82, f'N = {num_points}', transform=ax.transAxes, fontsize=12)
+    axes[i].text(0.05, 0.65, f'N = {num_points}', transform=axes[i].transAxes, fontsize=8)
 
     # Perform linear regression with NaN handling
     mask = ~np.isnan(site_data['UV-Vis']) & ~np.isnan(site_data['HIPS'])
@@ -114,26 +124,33 @@ for site_name in site_names:
     else:
         # Add linear regression line and text
         sns.regplot(x='UV-Vis', y='HIPS', data=site_data, scatter=False, ci=None,
-                    line_kws={'color': 'k', 'linestyle': '-', 'linewidth': 1}, ax=ax)
+                    line_kws={'color': 'k', 'linestyle': '-', 'linewidth': 1}, ax=axes[i])
 
         # Change the sign of the intercept for display
-        intercept_display = abs(intercept)  # Use abs() to ensure a positive value
-        intercept_sign = '-' if intercept < 0 else '+'  # Determine the sign for display
-
-        # Update the text line with the adjusted intercept
-        ax.text(0.05, 0.85, f"y = {slope:.2f}x {intercept_sign} {intercept_display:.2f}\n$r^2$ = {r_value ** 2:.2f}",
-                transform=ax.transAxes, fontsize=12)
+        intercept_display = abs(intercept)
+        intercept_sign = '-' if intercept < 0 else '+'
+        axes[i].text(0.05, 0.75, f"y = {slope:.2f}x {intercept_sign} {intercept_display:.2f}\n$r^2$ = {r_value ** 2:.2f}",
+                     transform=axes[i].transAxes, fontsize=8)
 
     # Set plot title as site name
-    ax.set_title(site_name, fontname='Arial', fontsize=14)
+    axes[i].set_title(site_name, fontname='Arial', fontsize=10)
 
     # Set x and y labels for the subplot
-    ax.set_xlabel('UV-Vis Black Carbon (µg/m$^3$)', fontsize=14, fontname='Arial')
-    ax.set_ylabel('HIPS Black Carbon (µg/m$^3$)', fontsize=14, fontname='Arial')
+    axes[i].set_xlabel('')
+    axes[i].set_ylabel('')
+    # axes[i].set_xlabel('UV-Vis Black Carbon Concentration (µg/m$^3$)', fontsize=8, fontname='Arial')
+    # axes[i].set_ylabel('HIPS Black Carbon Concentration (µg/m$^3$)', fontsize=8, fontname='Arial')
 
-    # Adjust vertical distance among subplots
-    fig.tight_layout()
+# set x and y labels above the subplots
+fig.text(0.5, 0.05, 'UV-Vis Black Carbon Concentration (µg/m$^3$)', ha='center', va='center', fontsize=14, fontname='Arial')
+fig.text(0.03, 0.5, 'HIPS Black Carbon Concentration (µg/m$^3$)', ha='center', va='center', rotation='vertical', fontsize=14, fontname='Arial')
 
-    # Save each subplot as an individual image
-    plt.savefig(os.path.join(Out_dir_path, "BC_{site_name}.tiff"), format="tiff", dpi=300)
-    plt.show()
+# Adjust vertical distance among subplots
+fig.tight_layout()
+
+# Adjust the figure size to accommodate the common x and y labels
+fig.subplots_adjust(bottom=0.12, left=0.06)
+
+# Save the combined plot as an image (optional)
+plt.savefig(os.path.join(Out_dir_path, "BC_Concentration_Comparison_by_Site.tiff"), format="tiff", dpi=300)
+plt.show()
