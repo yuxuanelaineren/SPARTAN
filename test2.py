698682abf50eff8db4d3186127705a1d735b9504
yuxuanelaineren <myyuxuanren@gmail.com>
2024-06-05 18:30:47 -0500

    cannot save as tiff

--- test2.py
+++ test2.py
@@ -1,5 +1,249 @@
-import h5py
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-file = h5py.File('data.h5', 'r')
-# perform operations
-file.close()
+import os
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs  # cartopy must be >=0.19
+import xarray as xr
+import cartopy.feature as cfeature
+import pandas as pd
+from gamap_colormap import WhGrYlRd
+import calendar
+import numpy as np
+from scipy.spatial.distance import cdist
+import geopandas as gpd
+from shapely.geometry import Point
+from shapely.ops import nearest_points
+from matplotlib import font_manager
+import seaborn as sns
+from scipy import stats
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.colors as mcolors
+
+cres = 'C360'
+year = 2019
+species = 'BC'
+inventory = 'CEDS'
+deposition = 'noLUO'
+
+# Set the directory path
+sim_dir = '/Volumes/rvmartin2/Active/Shared/dandan.z/GCHP-v13.4.1/output-{}-{}/monthly/'.format(cres.lower(), deposition) # CEDS, noLUO
+# sim_dir = '/Volumes/rvmartin2/Active/Shared/dandan.z/GCHP-v13.4.1/output-{}/monthly/'.format(cres.lower()) # HTAP, LUO
+# sim_dir = '/Volumes/rvmartin/Active/dandan.z/AnalData/WUCR3-C360/' # EDGAR, LUO
+# sim_dir = '/Volumes/rvmartin/Active/ren.yuxuan/BC_Comparison/WUCR3-C360/' # EDGAR, LUO
+# sim_dir = '/Volumes/rvmartin/Active/ren.yuxuan/BC_Comparison/{}_{}_{}_{}/'.format(cres.lower(), inventory, deposition, year) # C720, HTAP, LUO
+obs_dir = '/Volumes/rvmartin/Active/SPARTAN-shared/Analysis_Data/Master_files/'
+site_dir = '/Volumes/rvmartin/Active/SPARTAN-shared/Site_Sampling/'
+out_dir = '/Volumes/rvmartin/Active/ren.yuxuan/BC_Comparison/{}_{}_{}_{}/'.format(cres.lower(), inventory, deposition, year)
+
+def get_city_index(city):
+    for region, cities in region_mapping.items():
+        if city in cities:
+            return cities.index(city)
+    return float('inf')  # If city is not found, place it at the end
+def get_region_for_city(city):
+    for region, cities in region_mapping.items():
+        if city in cities:
+            return region
+    print(f"Region not found for city: {city}")
+    return None
+def map_city_to_color(city):
+    for region, cities in region_mapping.items():
+        if city in cities:
+            city_index = cities.index(city) % len(region_colors[region])
+            assigned_color = region_colors[region][city_index]
+            print(f"City: {city}, Region: {region}, Assigned Color: {assigned_color}")
+            return assigned_color
+    print(f"City not found in any region: {city}")
+    return (0, 0, 0)
+def map_city_to_marker(city):
+    for region, cities in region_mapping.items():
+        if city in cities:
+            city_index = cities.index(city) % len(region_colors[region])
+            assigned_marker = region_markers[region][city_index]
+            print(f"City: {city}, Region: {region}, Assigned Marker: {assigned_marker}")
+            return assigned_marker
+    print(f"City not found in any region: {city}")
+    return (0, 0, 0)
+def map_city_to_marker(city):
+    for region, cities in region_mapping.items():
+        if city in cities:
+            city_index = cities.index(city)
+            assigned_marker = region_markers[region][city_index % len(region_markers[region])]
+            return assigned_marker
+    return None
+# Read the file
+# compr_df = pd.read_excel(os.path.join(out_dir, '{}_{}_{}_Sim_vs_SPARTAN_{}_{}_Summary.xlsx'.format(cres, inventory, deposition, species, year)), sheet_name='Mon')
+compr_df = pd.read_excel(os.path.join(out_dir, '{}_{}_{}_Sim_vs_SPARTAN_{}_{}_Summary.xlsx'.format(cres, inventory, deposition, species, year)), sheet_name='Annual')
+compr_df['obs'] = 0.6 * compr_df['obs']
+
+# Print the names of each city
+unique_cities = compr_df['city'].unique()
+for city in unique_cities:
+    print(f"City: {city}")
+
+# Classify 'city' based on 'region'
+region_mapping = {
+    'North America': ['Downsview', 'Halifax', 'Kelowna', 'Lethbridge', 'Sherbrooke', 'Baltimore', 'Bondville', 'Mammoth Cave', 'Norman', 'Pasadena', 'Fajardo', 'Mexico City'],
+    'Australia': ['Melbourne'],
+    'East Asia': ['Beijing', 'Seoul', 'Ulsan', 'Kaohsiung', 'Taipei'],
+    'Central Asia': ['Abu Dhabi', 'Haifa', 'Rehovot'],
+    'South Asia': ['Dhaka', 'Bandung', 'Delhi', 'Kanpur', 'Manila', 'Singapore', 'Hanoi'],
+    'Africa': ['Bujumbura', 'Addis Ababa', 'Ilorin', 'Johannesburg', 'Pretoria'],
+    'South America': ['Buenos Aires', 'Santiago', 'Palmira'],
+}
+region_mapping = {region: [city for city in cities if city in unique_cities] for region, cities in region_mapping.items()}
+
+# Define custom palette for each region with 5 shades for each color, https://rgbcolorpicker.com/0-1
+region_colors = {
+    'North America': [
+        (0, 0, 0.6),  (0, 0, 1), (0, 0.27, 0.8), (0.4, 0.5, 0.9), (0.431, 0.584, 1), (0.7, 0.76, 0.9)
+    ],  # Blue shades
+    'Central Asia': [
+        (0.58, 0.1, 0.81), (0.66, 0.33, 0.83), (0.9, 0.4, 1), (0.73, 0.44, 0.8), (0.8, 0.55, 0.77), (0.88, 0.66, 0.74)
+    ],  # Purple shades
+    'Australia': [
+        (0.6, 0.4, 0.2)
+    ],  # Brown
+    'East Asia': [
+        (0, 0.5, 0), (0, 0.8, 0), (0, 1, 0), (0.56, 0.93, 0.56), (0.8, 0.9, 0.8)
+    ],  # Green shades
+    'South Asia': [
+        (0.5, 0, 0), (0.8, 0, 0), (1, 0, 0), (1, 0.4, 0.4), (0.9, 0.6, 0.6)
+    ],  # Red shades
+    'Africa': [
+        (1, 0.4, 0), (1, 0.6, 0.14), (1, 0.63, 0.48), (1, 0.85, 0.73), (1, 0.96, 0.85)
+    ], # Orange shades
+    'South America': [
+        (1, 0.16, 0.827), (1, 0.42, 0.70), (0.8, 0.52, 0.7), (0.961, 0.643, 0.804), (1, 0.64, 0.64), (1, 0.76, 0.48)
+    ]  # Pink shades
+}
+
+# Create an empty list to store the city_palette for each city
+city_palette = []
+city_color_match = []
+# Iterate over each unique city and map it to a gradient
+for city in unique_cities:
+    city_color = map_city_to_color(city)
+    if city_color is not None:
+        city_palette.append(city_color)
+        city_color_match.append({'city': city, 'color': city_color})  # Store both city name and color
+print("City Palette:", city_palette)
+
+# Define custom palette for each region with 5 shades for each color
+# markers = ['o', 's', '^', 'v', '<', '>', 'D', '*', 'H', '+', 'x', 'P', 'p', 'X', '1', '2', '3', '4']
+# ['o', 'H', 'p', 's', '^', 'P']
+region_markers = {
+    'North America': ['o', '^', 's', 'p', 'H', '*'],
+    'Australia': ['o', '^', 's', 'p', 'H', '*'],
+    'East Asia': ['o', '^', 's', 'p', 'H', '*'],
+    'Central Asia': ['o', '^', 's', 'p', 'H', '*'],
+    'South Asia': ['o', '^', 's', 'p', 'H', '*'],
+    'Africa': ['o', '^', 's', 'p', 'H', '*'],
+    'South America': ['o', '^', 's', 'p', 'H', '*'],
+}
+
+# Create an empty list to store the city_marker for each city
+city_marker = []
+city_marker_match = []
+
+# Iterate over each unique city and map it to a marker
+for city in unique_cities:
+    marker = map_city_to_marker(city)
+    if marker is not None:
+        city_marker.append(marker)
+        city_marker_match.append({'city': city, 'marker': marker})
+
+print("City Marker:", city_marker)
+
+# Define the range of x-values for the two segments
+x_range_1 = [compr_df['obs'].min(), 0.7]
+x_range_2 = [0.7, compr_df['obs'].max()]
+# x_range_1 = [compr_df['obs'].min(), 2.4*0.6]
+# x_range_2 = [2.4*0.6, compr_df['obs'].max()]
+x_range = [compr_df['obs'].min(), compr_df['obs'].max()]
+# Create figure and axes objects
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Create scatter plot with white background, black border, and no grid
+sns.set(font='Arial')
+scatterplot = sns.scatterplot(x='obs', y='sim', data=compr_df, hue='city', palette=city_palette, s=80, alpha=1, edgecolor='k', style='city',  markers=city_marker)
+scatterplot.set_facecolor('white')  # set background color to white
+border_width = 1
+for spine in scatterplot.spines.values():
+    spine.set_edgecolor('black')  # set border color to black
+    spine.set_linewidth(border_width)  # set border width
+scatterplot.grid(False)  # remove the grid
+
+# Sort the unique_cities list based on their appearance in region_mapping
+unique_cities_sorted = sorted(unique_cities, key=get_city_index)
+
+# Create legend with custom order
+sorted_city_color_match = sorted(city_color_match, key=lambda x: (
+    list(region_mapping.keys()).index(get_region_for_city(x['city'])),
+    region_mapping[get_region_for_city(x['city'])].index(x['city'])
+))
+
+# Create legend handles with both color and marker for each city
+legend_handles = []
+for city_info in sorted_city_color_match:
+    city = city_info['city']
+    color = city_info['color']
+    marker = map_city_to_marker(city)
+    if marker is not None:
+        handle = plt.Line2D([0], [0], marker=marker, color=color, linestyle='', markersize=8, label=city)
+        legend_handles.append(handle)
+
+# Create legend with custom handles
+legend = plt.legend(handles=legend_handles, facecolor='white', bbox_to_anchor=(1.03, 0.50), loc='center left', fontsize=11.5)
+legend.get_frame().set_edgecolor('black')
+
+# Set title, xlim, ylim, ticks, labels
+# plt.title(f'GCHP-v13.4.1 {cres.lower()} {inventory} {deposition} vs SPARTAN', fontsize=16, fontname='Arial', y=1.03)  # PM$_{{2.5}}$
+plt.xlim([-0.5, 10])
+plt.ylim([-0.5, 10])
+plt.xticks([0, 2, 4, 6, 8, 10], fontname='Arial', size=18)
+plt.yticks([0, 2, 4, 6, 8, 10], fontname='Arial', size=18)
+scatterplot.tick_params(axis='x', direction='out', width=1, length=5)
+scatterplot.tick_params(axis='y', direction='out', width=1, length=5)
+
+# Add 1:1 line with grey dash
+plt.plot([compr_df['obs'].min(), 10], [compr_df['obs'].min(), 10],
+         color='grey', linestyle='--', linewidth=1)
+
+# Perform linear regression for the first segment
+mask_1 = (compr_df['obs'] >= x_range_1[0]) & (compr_df['obs'] <= x_range_1[1])
+slope_1, intercept_1, r_value_1, p_value_1, std_err_1 = stats.linregress(compr_df['obs'][mask_1], compr_df['sim'][mask_1])
+# Perform linear regression for the second segment
+mask_2 = (compr_df['obs'] >= x_range_2[0]) & (compr_df['obs'] <= x_range_2[1])
+slope_2, intercept_2, r_value_2, p_value_2, std_err_2 = stats.linregress(compr_df['obs'][mask_2], compr_df['sim'][mask_2])
+# Plot regression lines
+sns.regplot(x='obs', y='sim', data=compr_df[mask_1],
+            scatter=False, ci=None, line_kws={'color': 'blue', 'linestyle': '-', 'linewidth': 1.5}, ax=ax)
+
+# Add text with linear regression equations and other statistics
+intercept_display_1 = abs(intercept_1)
+intercept_display_2 = abs(intercept_2)
+intercept_sign_1 = '-' if intercept_1 < 0 else '+'
+intercept_sign_2 = '-' if intercept_2 < 0 else '+'
+plt.text(0.05, 0.76, f'y = {slope_1:.2f}x {intercept_sign_1} {intercept_display_1:.2f}\n$r^2$ = {r_value_1 ** 2:.2f}',
+         transform=scatterplot.transAxes, fontsize=18, color='blue')
+plt.text(0.05, 0.51, f'y = {slope_2:.2f}x {intercept_sign_2} {intercept_display_2:.2f}\n$r^2$ = {r_value_2 ** 2:.2f}',
+         transform=scatterplot.transAxes, fontsize=18, color='red')
+
+# Add the number of data points for each segment
+num_points_1 = mask_1.sum()
+plt.text(0.05, 0.70, f'N = {num_points_1}', transform=scatterplot.transAxes, fontsize=18, color='blue')
+num_points_2 = mask_2.sum()
+plt.text(0.05, 0.45, f'N = {num_points_2}', transform=scatterplot.transAxes, fontsize=18, color='red')
+
+# Set labels
+plt.xlabel('HIPS Measured Black Carbon (µg/m$^3$)', fontsize=18, color='black', fontname='Arial')
+plt.ylabel('GCHP C360 Black Carbon (µg/m$^3$)', fontsize=18, color='black', fontname='Arial')
+
+# Show the plot
+plt.tight_layout()
+# plt.savefig(out_dir + 'FigS3_Scatter_c720_vs_c360_201801.svg', dpi=300)
+
+plt.show()
